18e85c30136e45dd21095b52a0da402ff31f20f4
Flynt-251 <finn.mcgoldrick@warwick.ac.uk>
2026-03-06 18:59:28 +0000

    ACTUALLY fixed Discover this time (bc Alex broke it again)

--- home/startsession.sh
+++ home/startsession.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+sudo flatpak remote-delete flathub
 flatpak remote-add --user --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+flatpak install --user org.vinegarhq.Sober
 dbus-run-session startplasma-x11
