80c01effc54c177d0ffa7e2eecf96dea53178d6c
raven0034 <monkeyboy2805@gmail.com>
2026-07-15 18:06:53 +0100

    Ditch __GL_THREADED_OPTIMIZATIONS (fixes issue e.g. with Sober)
    Ditch failed mime update as well

--- home/startsession.sh
+++ home/startsession.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 export __EGL_VENDOR_LIBRARY_FILENAMES=/usr/share/glvnd/egl_vendor.d/10_nvidia.json
 #nvidia-settings --assign CurrentMetaMode="nvidia-auto-select +0+0 {ForceFullCompositionPipeline=On}" || true
-export __GL_THREADED_OPTIMIZATIONS=1
 export __GL_SHADER_DISK_CACHE_SKIP_CLEANUP=1
-
-update-mime-database /usr/share/mime
 
 flatpak --user remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 
