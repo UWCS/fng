28711051bd82a45bc14e5de23b118208e8ce0376
raven0034 <monkeyboy2805@gmail.com>
2026-07-29 16:09:51 +0100

    New install script & nuke xstartup

--- install.sh
+++ install.sh
@@ -1,27 +1,18 @@
 #!/bin/bash
-set -e
 
-# Install with
-# curl -s https://raw.githubusercontent.com/UWCS/fng/main/install.sh | sh
-
-# Download files
-URL="https://raw.githubusercontent.com/UWCS/fng/main"
-curl "${URL}/host/storage.conf" --create-dirs -o "${HOME}/.config/containers/storage.conf"
-curl "${URL}/host/.xsession" --create-dirs -o "${HOME}/.xsession"
-curl "${URL}/host/xstartup" --create-dirs -o "${HOME}/.vnc/xstartup"
-chmod +x ~/.xsession
-
-# Download distrobox
-# Find the distrobox binary
-if command -v distrobox &> /dev/null
-then
-    distrobox=$(which distrobox)
-elif [ -f ~/.local/bin/distrobox ]
-then
-    distrobox=~/.local/bin/distrobox
+if [ -f ~/.config/containers/storage.conf ]; then
+    echo "Skipped: ~/.config/containers/storage.conf already exists"
 else
-    echo "Installing distrobox"
-    curl -s https://raw.githubusercontent.com/89luca89/distrobox/main/install | sh -s -- --next --prefix ~/.local
+    mkdir -p ~/.config/containers
+    cp ./host/storage.conf ~/.config/containers/storage.conf
 fi
 
-echo "Installed Sucessfully"
+if [ -f ~/.xsession ]; then
+    echo "Skipped: ~/.xsession already exists"
+else
+    target=$(dirname "$(readlink -f '$0')")
+    echo -e "#!/bin/bash\ncd ${target}\n./host/.xsession" > ~/.xsession
+    chmod +x ~/.xsession
+fi
+
+echo "Done"
